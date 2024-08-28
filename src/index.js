b297e4d47e7eb6ffa97ecd5b9ca3b515381811f4
lsy1128 <lsy1128@naonworks.com>
2024-08-28 15:45:12 +0900

    Vanilla Redux - TO DO LIST 구현 시작

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
 import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
-
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if ((action.type = "MINUS")) {
-    return count - 1;
-  } else {
-    return count;
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-// message 를 send 하는 방법 = dispatch 사용
-// message 는 action에 넣으면 되고, action을 체크하면 된다.
-// countStore.dispatch({ type: "ADD" });
-// countStore.dispatch({ type: "MINUS" });
-
-const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
-const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
